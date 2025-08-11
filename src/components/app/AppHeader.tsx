6e6613b3e2a5d36f8352f1004abb97fa4178630f
minhcu <minhdong1831999@gmail.com>
2025-08-11 19:18:22 +0700

    2nd commit

---
+++ src/components/app/AppHeader.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button"
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { IconPlus } from "@tabler/icons-react"
+import { Input } from "@/components/ui/input"
+
+export function AppHeader() {
+    return (
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+            <div className="w-full p-6 border-b bg-background">
+                <div className="flex items-center justify-between mb-4 w-full">
+                    <div className="flex items-center gap-3">
+                        <SidebarTrigger className="-ml-1" />
+                        <div>
+                            <h1 className="text-2xl font-bold">Backlog</h1>
+                            <p className="text-muted-foreground">Manage user stories, sprints and tasks</p>
+                        </div>
+                    </div>
+
+                    <div className="flex gap-2">
+                        <Button variant="outline" className="mt-2">
+                            <IconPlus />
+                            <span className="text-sm">Create sprint</span>
+                        </Button>
+
+                        <Button variant="outline" className="mt-2 ml-2">
+                            <IconPlus />
+                            <span className="text-sm">Add task</span>
+                        </Button>
+
+                        <Button color="primary" className="mt-2 ml-2">
+                            <IconPlus />
+                            <span className="text-sm">Add user story</span>
+                        </Button>
+                    </div>
+                </div>
+
+                <div className="flex gap-4 items-center">
+                    <Input placeholder="Search user stories..." />
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,3 +1,4 @@
+import { AppHeader } from "@/components/app/AppHeader";
 import { AppSidebar } from "@/components/app/AppSidebar";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 
@@ -13,7 +14,7 @@ export function DefaultLayout(
             <SidebarProvider>
                 <AppSidebar variant="inset" />
                 <SidebarInset>
-                    {/* <SiteHeader /> */}
+                    <AppHeader />
                     <div className="flex flex-1 flex-col">
                         <div className="@container/main flex flex-1 flex-col gap-2">
                             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
